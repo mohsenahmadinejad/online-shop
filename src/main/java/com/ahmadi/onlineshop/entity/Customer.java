10c5ec1cf63e6a1fd5b2b7381a2bf9c4c7527cac
Mohsen Ahmadinejad <ahmadi.mohsen.s@gmail.com>
2025-09-08 17:23:46 +0330

    feat: added database connection - added Customer entity

---
+++ src/main/java/com/ahmadi/onlineshop/entity/Customer.java
@@ -0,0 +1,28 @@
+package com.ahmadi.onlineshop.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "t_customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+    private String phone;
+    private String address;
+
+}
