e82e7d1a4489107a53741f304788c58c606a8b94
Mohsen Ahmadinejad <ahmadi.mohsen.s@gmail.com>
2025-09-08 17:29:34 +0330

    feat: added  Customer repository

---
+++ src/main/java/com/ahmadi/onlineshop/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.ahmadi.onlineshop.repository;
+
+import com.ahmadi.onlineshop.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
