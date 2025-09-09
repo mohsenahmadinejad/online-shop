ae2af2d62f5acef35c1b8eca8b8d307498d4acb6
Mohsen Ahmadinejad <ahmadi.mohsen.s@gmail.com>
2025-09-09 20:21:43 +0330

    fix: bug fixed

--- src/main/java/com/ahmadi/onlineshop/repository/StockRepository.java
+++ src/main/java/com/ahmadi/onlineshop/repository/StockRepository.java
@@ -1,6 +1,7 @@
 package com.ahmadi.onlineshop.repository;
 
+import com.ahmadi.onlineshop.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockRepository extends JpaRepository<StockRepository,Long> {
+public interface StockRepository extends JpaRepository<Stock,Long> {
 }
