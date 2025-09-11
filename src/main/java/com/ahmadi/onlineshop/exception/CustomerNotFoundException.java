a98f2f77bce01595533ccc99143a5b2ced4bb6ff
Mohsen Ahmadinejad <ahmadi.mohsen.s@gmail.com>
2025-09-11 23:27:54 +0330

    feat: -dockerized app and mysql - added Dockerfile

---
+++ src/main/java/com/ahmadi/onlineshop/exception/CustomerNotFoundException.java
@@ -0,0 +1,12 @@
+package com.ahmadi.onlineshop.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends RuntimeException {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
