dbc19301a16b38e92c2a1a62e03785c1ea06492b
mohsenahmadinejad <ahmadi.mohsen.s@gmail.com>
2025-09-12 11:12:40 +0330

    feat: get current user from SecurityContextHolder for auditing

--- src/main/java/com/ahmadi/onlineshop/config/AuditorAwareImpl.java
+++ src/main/java/com/ahmadi/onlineshop/config/AuditorAwareImpl.java
@@ -1,6 +1,8 @@
 package com.ahmadi.onlineshop.config;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -8,12 +10,11 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        if (authentication == null || !authentication.isAuthenticated()) {
-//            return Optional.empty();
-//        }
-//        return Optional.of(authentication.getName());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+        return Optional.of(authentication.getName());
 
-        return Optional.of("admin");
     }
 }
